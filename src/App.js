40939a222336ad805fb5f5303fa4898b1a047828
DedInsade <skok2783@gmail.com>
2020-06-06 23:45:39 +0700

    ////

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Footer from './components/footer/footer';
+import Main from './components/main/main';
+import Header from './components/header/header';
+
+const App = () => {
+  return ( 
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+        <ul>
+            <li>Thing</li>
+            <li>Thing</li>
+            <li>Thing</li>
+        </ul>
+        </div>
+    );
+}
+
+export default Nav;
